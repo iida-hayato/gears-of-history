fc1a25202b0752c1416551a11e4714186d55e2a0
hayato.iida <hayato.iida.0213@gmail.com>
2025-08-25 20:26:33 +0900

    production調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
-import { Client } from 'boardgame.io/react'
-import { GearsOfHistory } from './game/game'
+import {Client} from 'boardgame.io/react'
+import {GearsOfHistory} from './game/game'
 import Board from './ui/Board'
+import {Debug} from "boardgame.io/debug";
 
-const BGClient = Client({ game: GearsOfHistory, board: Board, numPlayers: 4 })
-export default function App(){ return <BGClient playerID={'0'} /> }
+const url = new URL(window.location.href);
+const enableDebug =
+    url.searchParams.has('debug') ||
+    import.meta.env.MODE !== 'production';
+const playerCount = 
+    parseInt(url.searchParams.get('player') ?? "4");
+
+const BGClient = Client({
+    game: GearsOfHistory,
+    board: Board,
+    numPlayers: playerCount,
+    debug: enableDebug ? {impl: Debug} : false,
+})
+export default function App() {
+    return <BGClient playerID={'0'}/>
+}
